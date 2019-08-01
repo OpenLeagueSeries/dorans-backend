d78f193617856525d68494abd4190438b0b85a6b
Zachary Chay-Dolan <zacharia.cd@gmail.com>
2019-08-01 16:38:57 -0400

    initial react app

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Leaderboard from './leaderboard/board';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Doran's trophy case
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
