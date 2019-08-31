9f009ae422465a2f6138ab5acefb040e545c9e1e
Zachary Chay-Dolan <zacharia.cd@gmail.com>
2019-08-30 21:49:29 -0400

    change to url

--- backend/doran/urls.py
+++ backend/doran/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from doran.views import char_count
+from doran.views import char_count, riot
 
 urlpatterns = [
     path('admin/', admin.site.urls),
