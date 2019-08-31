cc91994218acf9554a6659e7d1bb2099563135c7
Zachary Chay-Dolan <zacharia.cd@gmail.com>
2019-08-30 21:22:44 -0400

    using a new call for character count

--- frontend/src/summonerForm/form.js
+++ frontend/src/summonerForm/form.js
@@ -2,6 +2,14 @@ import React, { useState } from 'react';
 
 function SummonerForm() {
     const [summ, setSumm] = useState('');
+    const [count, setCount] = useState(0);
+
+    const checkCharCount = (summ) => {
+      fetch(`/char_count?text=${summ}`)
+        .then(response => response.json())
+        .then(data => setCount(data.count))
+        .catch(err => console.log(err));
+    }
 
     return (
       <div>
@@ -9,8 +17,13 @@ function SummonerForm() {
         <form>
         <label>
           Summoner Name:
-          <input type="text" name="summoner" value={summ} onChange={(ev) => setSumm(ev.target.value)}/>
+          <input type="text" name="summoner" value={summ} onChange={(ev) =>
+              {
+                setSumm(ev.target.value);
+                checkCharCount(ev.target.value);
+              }}/>
         </label>
+        {count}
         <input type="submit" value="Submit" />
 
         </form>
