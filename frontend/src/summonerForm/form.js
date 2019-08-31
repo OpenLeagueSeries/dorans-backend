320195bb936bb2f60869a5277172d7c594a64202
Zachary Chay-Dolan <zacharia.cd@gmail.com>
2019-08-30 21:44:36 -0400

    added some api calls

--- frontend/src/summonerForm/form.js
+++ frontend/src/summonerForm/form.js
@@ -3,12 +3,21 @@ import React, { useState } from 'react';
 function SummonerForm() {
     const [summ, setSumm] = useState('');
     const [count, setCount] = useState(0);
+    const [level, setLevel] = useState(0);
+    const [region, setRegion] = useState('');
 
     const checkCharCount = (summ) => {
       fetch(`/char_count?text=${summ}`)
         .then(response => response.json())
         .then(data => setCount(data.count))
         .catch(err => console.log(err));
+    }
+
+    const getData = (summ) => {
+      fetch(`/riot?summoner=${summ}`)
+      .then(response => response.json())
+      .then(data => { setLevel(data.level); setRegion(data.level); })
+      .catch(err => console.log(err));
     }
 
     return (
@@ -21,9 +30,12 @@ function SummonerForm() {
               {
                 setSumm(ev.target.value);
                 checkCharCount(ev.target.value);
+                getData(ev.target.value);
               }}/>
         </label>
         {count}
+        {level}
+        {region}
         <input type="submit" value="Submit" />
 
         </form>
